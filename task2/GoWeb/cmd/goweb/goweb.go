1619bb7a0e22071ab81b1c9a936b90b6e9852b5a
Starlexxx <apavlovskiy088@gmail.com>
2023-07-02 12:03:15 +0300

    Change project structure.

---
+++ task2/GoWeb/cmd/goweb/goweb.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"goweb/pkg/crawler"
+	"goweb/pkg/crawler/spider"
+	"log"
+	"strings"
+)
+
+var (
+	webSites = []string{
+		"https://go.dev",
+		"https://golang.org",
+	}
+	searchWord string
+)
+
+func init() {
+	flag.StringVar(&searchWord, "s", "", "word to search in scanned documents")
+	flag.Parse()
+}
+
+func main() {
+	c := spider.New()
+	var docs []crawler.Document
+
+	for _, url := range webSites {
+		data, err := c.Scan(url, 2)
+		if err != nil {
+			log.Printf("GoWeb: %s", err)
+			continue
+		}
+		docs = append(docs, data...)
+	}
+
+	for _, d := range docs {
+		if searchWord == "" {
+			log.Printf("GoWeb: %s - %s", d.URL, d.Title)
+		} else if searchWord != "" && strings.Contains(d.URL, searchWord) {
+			log.Printf("GoWeb: %s - %s", d.URL, d.Title)
+		}
+	}
+}
